2c26481d7050b04db0b3c19e050f8ab5bd0a3f96
Michelle Moon <michellemoon236@gmail.com>
2019-05-08 01:26:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,8 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash[:name] = @name
+    student_hash[name] = @name
+    
   end
 
   def self.create_from_collection(students_array)
