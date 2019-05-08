36f81a1768263572153eb5aa7972f661fb17d050
Michelle Moon <michellemoon236@gmail.com>
2019-05-08 01:37:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,7 +26,7 @@ class Student
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |attribute, value|
-      self[attribute] = value
+      self[attribute.to_sym] = value
     end
     self
   end
